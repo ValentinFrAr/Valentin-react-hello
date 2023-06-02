21065b30aa1e24dd31a89b1f1ca154e4902ee22f
ValentinFrAr <116929850+ValentinFrAr@users.noreply.github.com>
2023-06-02 03:52:33 +0000

    react

--- src/js/component/Header.jsx
+++ src/js/component/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   <div class="card-body">
     <h5 class="card-title fs-1">A Warm Welcome</h5>
     <p class="card-text fs-3">Re facil es hacer esto</p>
-    <a href="#" class="btn btn-primary">Call to action</a>
+    <a href="#" class="btn btn-danger">Call to action</a>
   </div>
 </div>
 	
